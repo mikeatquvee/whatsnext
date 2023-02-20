4df75925e5db474fe5c4e8e51198c46a3783e73f
Mike <mike@quvee.io>
2023-02-20 12:23:15 +0000

    initial theme changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	import '@skeletonlabs/skeleton/styles/all.css';
-	import '../app.postcss';
+	import '../qv_theme.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 </script>
 
