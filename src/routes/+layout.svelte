399b4a521390ac9d4e57ebc83565df63aa5166b6
Mike <mike@quvee.io>
2023-02-20 17:30:30 +0000

    we have tokens

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,46 +1,19 @@
 <script>
-	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
-	import '@skeletonlabs/skeleton/styles/all.css';
-	import '../qv_theme.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+  // import "@skeletonlabs/skeleton/themes/theme-skeleton.css";
+  import "../qv_theme.postcss";
+  import "@skeletonlabs/skeleton/styles/all.css";
+  import "../app.postcss";
+  import { AppShell, AppBar } from "@skeletonlabs/skeleton";
 </script>
 
 <!-- App Shell -->
 <AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-	<!-- Page Route Content -->
-	<slot />
+  <svelte:fragment slot="header">
+    <!-- App Bar -->
+    <AppBar background="bg-surface-100-800-token">
+      <strong class="text-xl ">Whats Next?</strong>
+    </AppBar>
+  </svelte:fragment>
+  <!-- Page Route Content -->
+  <slot />
 </AppShell>
